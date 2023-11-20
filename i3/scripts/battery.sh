cd3140571f9045cd2e7379d976a38548fd6d3901
Favot Corentin <70208301+Favot@users.noreply.github.com>
2023-11-20 10:12:36 +0100

    Update color for battery icon

--- i3/scripts/battery.sh
+++ i3/scripts/battery.sh
@@ -3,24 +3,42 @@
 capacity=$(cat /sys/class/power_supply/BATT/capacity)
 status=$(cat /sys/class/power_supply/BATT/status)
 
-batteryCapacityIcon() {
+getBatteryIcon() {
 	if [ $1 -ge 90 ]; then
-		echo "" # Icon for 90% and above
+		echo "" # batteryIcon for 90% and above
 	elif [ $1 -ge 75 ]; then
-		echo "" # Icon for 75% to 89%
+		echo "" # batteryIcon for 75% to 89%
 	elif [ $1 -ge 50 ]; then
-		echo "" # Icon for 50% to 74%
+		echo "" # batteryIcon for 50% to 74%
 	elif [ $1 -ge 25 ]; then
-		echo "" # Icon for 25% to 49%
+		echo "" # batteryIcon for 25% to 49%
 	else
-		echo ' ' # Icon for 0% to 24%
+		echo ' ' # batteryIcon for 0% to 24%
 	fi
 }
 
-icon=$(batteryCapacityIcon $capacity)
+getChargingIcon() {
+	if [ "$status" = "Charging" ]; then
+		echo ""
+	else
+		echo ""
+	fi
+}
 
-if [ "$status" = "Charging" ]; then
-	echo " $icon   $capacity %"
-else
-	echo "$icon   $capacity %" ,
-fi
+getColor() {
+	if [ $1 -lt 25 ]; then
+		echo "#ed8796"
+	else
+		echo "#8bd5ca"
+	fi
+}
+
+batteryIcon=$(getBatteryIcon $capacity)
+chargingIcon=$(getChargingIcon $status)
+color=$(getColor $capacity)
+
+display() {
+	echo "<span color='$color'>$chargingIcon $batteryIcon   $capacity %</span>"
+}
+
+display
