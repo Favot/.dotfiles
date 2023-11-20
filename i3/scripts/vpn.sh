6e126edeb79b8cd646528329039641a118d4a463
Favot Corentin <70208301+Favot@users.noreply.github.com>
2023-11-20 09:49:54 +0100

    Update colors vpn status

--- i3/scripts/vpn.sh
+++ i3/scripts/vpn.sh
@@ -4,9 +4,9 @@ vpnCheck() {
 	status=$(expressvpn status)
 
 	if [[ $status == *"Connected"* ]]; then
-		echo '<span color="#00FF00">󰒄: On</span>'
+		echo '<span color="#a6da95">󰒄: On</span>'
 	else
-		echo "<span color='#ffa500'>󰒄: Off</span>"
+		echo "<span color='#ee99a0'>󰒄: Off</span>"
 	fi
 }
 
